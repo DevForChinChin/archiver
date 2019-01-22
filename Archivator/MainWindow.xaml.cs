f0b1f55225def66e90a538d49763cd6e704741d9
PositiveJew <PositiveJew@yandex.ru>
2019-01-23 01:09:48 +0300

    Работа с файлами теперь на SLSID ключах

--- Archivator/MainWindow.xaml.cs
+++ Archivator/MainWindow.xaml.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
 using System.Windows.Controls;
 using System.Drawing;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using Microsoft.WindowsAPICodePack.Shell;
 
 namespace Archivator
 {
@@ -68,6 +62,11 @@ namespace Archivator
     // расширение для Icon. Перевод в ImageSource. Нужно для отображения иконок в столбце "Имя"
     public static class ExtensionDefine
     {
+        /// <summary>
+        /// Конвертирует icon в ImageSource
+        /// </summary>
+        /// <param name="icon">Icon, которую нужно конвертировать</param>
+        /// <returns></returns>
         public static ImageSource ToImageSource(this Icon icon)
         {
             return Imaging.CreateBitmapSourceFromHBitmap(
@@ -79,31 +78,54 @@ namespace Archivator
     }
 
     // элемент ListView
+    /// <summary>
+    /// Класс, представляющий элемент ListView
+    /// </summary>
     public class ListItem
     {
         // полное имя файла. Иначе говоря абсолютный путь. Изменять нельзя.
+        /// <summary>
+        ///Абсолютный путь к файлу
+        /// </summary>
         public string FullName { get; }
         // Обычное имя файла. Отображается в столбце "Имя"
+        /// <summary>
+        ///Имя файла с расширением
+        /// </summary>
         public string Name { get; set; }
         // Иконка изображения
+        /// <summary>
+        ///Иконка, ассоциирующаяся с файлом
+        /// </summary>
         public ImageSource Image { get; set; }
-        // Дата последнего изменения
-        public string ChangeData { get; set; }
-        // Расширение файла
+        /// <summary>
+        ///Дата последнего изменения
+        /// </summary>
+        public DateTime ChangeData { get; set; }
+        /// <summary>
+        ///Расширение файла
+        /// </summary>
         public string Type { get; set; }
-        // Размер файла
+        /// <summary>
+        ///Размер файла
+        /// </summary>
         public string Size { get; set; }
 
+        // Конструктор, принимающий на вход класс FileInfo из пространства имён System.IO.
+        /// <summary>
+        /// Конструктор, принимающий информацию о файле и Иконку файла
+        /// </summary>
+        /// <param name="Inf">информация</param>
+        /// <param name="Ico">Иконка файла</param>
         public ListItem(FileInfo Inf, Icon Ico)
         {
-            // Конструктор принимающий на вход класс FileInfo из пространства имён System.IO.
             // Из него получаем абсолютный путь.
             FullName = Inf.FullName;
 
             // Имя файла с расширением
             Name = Inf.Name;
             // Дату последнего изменения
-            ChangeData = Inf.LastAccessTimeUtc.ToString();
+            ChangeData = Inf.LastWriteTime;
             // Расширение
             Type = Inf.Extension;
 
@@ -125,6 +147,63 @@ namespace Archivator
             Image = Ico.ToImageSource();
         }
 
+        /// <summary>
+        /// Конструктор, принимающий на вход информацию о файле, который берёт ассоциированную с этим файлом иконку
+        /// </summary>
+        /// <param name="info"></param>
+        public ListItem(FileInfo info)
+        {
+            // Из него получаем абсолютный путь.
+            FullName = info.FullName;
+
+            // Имя файла с расширением
+            Name = info.Name;
+            // Дату последнего изменения
+            ChangeData = info.LastWriteTime;
+            // Расширение
+            Type = info.Extension;
+
+            // И размер файла
+            // Но т.к он даётся в битах, нужно немного преобразовать строку.
+            // Недоделано. Надо размеры до ГБ сделать.
+            if (info.Length >= 1024)
+            {
+                // в одном килобайте 1024 байт.
+                Size = (info.Length / 1024).ToString() + " KB";
+            }
+            else
+            {
+                //Собственно если размер меньше 1024 байт
+                Size = info.Length.ToString() + " B";
+            }
+
+            // AssociatedIcon по полному имени файла, конвертируется в ImageSource
+            Image = Icon.ExtractAssociatedIcon(info.FullName).ToImageSource();
+        }
+
+        /// <summary>
+        /// ListItem из экземпляра ShellObject
+        /// </summary>
+        /// <param name="info"></param>
+        public ListItem(ShellObject info)
+        {
+            /* работает медленно.
+             * Зато чётко. Ну или почти: 
+             1 Размер файла не указывается. 
+             2 Некоторые иконки некорректно подгружаются. */
+
+            Name = info.Name;
+
+            Image = info.Thumbnail.BitmapSource;
+
+            // проверить ChangeData на null
+            ChangeData = info.Properties.System.DateModified.Value.Value;
+
+            Type = info.Properties.System.ItemType.Value;
+
+            Size = info.Properties.System.Size.Value.ToString();
+        }
+
         public override string ToString()
         {
             //↓         Плохо сформулированный бред       ↓\\
@@ -139,11 +218,18 @@ namespace Archivator
 
     public partial class MainWindow : Window
     {
+        ObservableCollection<ListItem> Entry;
         // метод получения папки с загрузками
         // Способ не идеальный так как берёт первый попавшийся диск
         // Отсортированы они по алфавиту, поэтому если папка с загрузками пользователя
         // находится на диске D, но при этом есть диск A,B,C, то поведение непредсказуемо.
         // Вообще exception может выбить. Надо бы обработать, но мне лень
+        #region Вспомогательные функции
+            /// <summary>
+            /// Возвращает путь к папке "Загрузки" первого по алфавиту диска.
+            /// Если не найдена, то возвращает первый по алфавиту логический диск
+            /// </summary>
+            /// <returns></returns>
         private string GetDownloadDir()
         {
             // Так как относительно много возьни со строками используем StringBuilder
@@ -155,12 +241,18 @@ namespace Archivator
             BBld.Append(@"Users\");
             BBld.Append(Environment.UserName);
             BBld.Append(@"\Downloads");
-            // 
-            return BBld.ToString();
+            
+            if(Directory.Exists(BBld.ToString()))
+            {
+                return BBld.ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
+        #endregion
 
-        // объявляю здесь потому что 
-        ObservableCollection<ListItem> Entry;
 
         public MainWindow()
         {
@@ -177,19 +269,27 @@ namespace Archivator
 
             Entry = new ObservableCollection<ListItem>();
 
+            // Получаем папку "Загрузки" текущего пользователя с помощью SLSID ключа
+            var Dir = (ShellFolder)ShellObject.FromParsingName("shell:::{374DE290-123F-4565-9164-39C4925E467B}");
+
             // получаем список абсолютных путей файлов в папке "Загрузки"
-            var Dirs = Directory.GetFiles(GetDownloadDir());
+            //var Dirs = Directory.GetFiles(@"C:\Users\mixap\Downloads");
+
             // для каждого пути
-            foreach (var path in Dirs)
+            foreach (var file in Dir)
             {
                 // добавляем новый элемент в список
-                Entry.Add(new ListItem(new FileInfo(path), System.Drawing.Icon.ExtractAssociatedIcon(path)));
+                Entry.Add(new ListItem(file));
             }
             // Выводим файлы в ListView
             Explorer.ItemsSource = Entry;
+            // вызываем Dispose для папки. Надо проверить, Dispose-ит ли он ShellObject-ы которые в себе хранит ( должен... но друг )
+            Dir.Dispose();
         }
 
-        // Кнопки управления (которые справа сверху) Объяснять что они делают не надо
+
+        #region кнопки Свернуть, Развернуть на весь экран, Закрыть
+
         private void FullSizeButton_Click(object sender, RoutedEventArgs e)
         {
             if (WindowState == WindowState.Normal)
@@ -207,25 +307,57 @@ namespace Archivator
             WindowState = WindowState.Minimized;
         }
 
+        #endregion
 
-        // TextBox (который справа от сдоровой стрелки) должен принимать на вход путь к папке или к файлу
+        // TextBox (который справа от здоровой стрелки) должен принимать на вход путь к папке или к файлу
         // и либо вывести список файлов в папке либо архивировать ( возможно отдельное окно сделать под это надо)
         // НЕ ДОДЕЛАНО. Exceptions | ввод вместе с файлом
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
+            
+            /* На всякий случай не удаляю. Я думаю надо вообще 2 версии сделать
+             * Одна будет через SLSID ключи, другая "По старинке" и проблему иконок решить путём "Не можем настоящую поставим дефолтную"
             // Когда TextBox находится в фокусе, мы ждём пока нажмут Enter.
             if (e.Key == Key.Enter)
             {
-                // здесь обновляем treeView
-                Entry.Clear();
-
                 // Берём текст из TextBox-а и опять составляем лист всех файлов
-                var Dirs = Directory.GetFiles(Navigator.Text);
-                foreach (var path in Dirs)
+                if(Directory.Exists(Navigator.Text))
                 {
-                    Entry.Add(new ListItem(new FileInfo(path), System.Drawing.Icon.ExtractAssociatedIcon(path)));
+                    // здесь обновляем treeView. Нужно добавить поддержку директорий. Короче конструктор с DirectoryInfo 
+                    Entry.Clear();
+                    var Dirs = Directory.GetFiles(Navigator.Text);
+                    foreach (var path in Dirs)
+                    {
+                        Entry.Add(new ListItem(new FileInfo(path)));
+                    }
+                    Explorer.ItemsSource = Entry;
                 }
-                Explorer.ItemsSource = Entry;
+                else
+                {
+                    MessageBox.Show($"Директория \"{Navigator.Text}\" не найдена или не существует");
+                }
+            }
+            */
+            //А нужен ли нам путь ?
+            if(e.Key == Key.Enter)
+            {
+                // Исключение надо ловить
+                if(Directory.Exists(Navigator.Text))
+                {
+                    var TargetFolder = (ShellFolder)ShellObject.FromParsingName(Navigator.Text);
+                    Entry.Clear();
+                    foreach (var item in TargetFolder)
+                    {
+                        // работай ок ?
+                        Entry.Add(new ListItem(item));
+                    }
+                    TargetFolder.Dispose();
+                }
+                else
+                {
+                    //Надо поменять стиль MessageBox
+                    MessageBox.Show("Директория не найдена");
+                }
             }
         }
 
@@ -234,6 +366,7 @@ namespace Archivator
         private void Explorer_Drop(object sender, DragEventArgs e)
         {
             // Абсолютный путь к файлу или к папке ( директории )
+            // почему оно имеет тип string[] ???
             string[] buffer = e.Data.GetData("FileName") as string[];
         }
 
@@ -254,7 +387,7 @@ namespace Archivator
             MessageBox.Show("Работает, успокойся");
         }
 
-        // не знаю насколько этот способ хорош но...
+        // сохранить выделенный до этого объект (Я себя понял). Надо попробовать MultiBinding
         private void ListViewItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             /* Это ломает Binding
