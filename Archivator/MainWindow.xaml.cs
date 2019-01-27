a6734356c458c8721d3a2b35778a0e0dd434d6e7
PositiveJew <PositiveJew@yandex.ru>
2019-01-28 01:19:02 +0300

    Асинхронность и не только.

--- Archivator/MainWindow.xaml.cs
+++ Archivator/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Collections.ObjectModel;
 using Microsoft.WindowsAPICodePack.Shell;
+using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Archivator
 {
@@ -74,6 +76,7 @@ namespace Archivator
            IntPtr.Zero,
            Int32Rect.Empty,
            BitmapSizeOptions.FromEmptyOptions());
+
         }
     }
 
@@ -177,10 +180,6 @@ namespace Archivator
         /// <param name="info"></param>
         public ListItem(ShellObject info)
         {
-            /* работает медленно.
-             * Зато чётко. Ну или почти: 
-             1 Некоторые иконки некорректно подгружаются. Вроде пофиксил */
-
             Name = info.Name;
 
             Image = info.Thumbnail.SmallBitmapSource;
@@ -190,7 +189,26 @@ namespace Archivator
 
             Type = info.Properties.System.ItemType.Value;
 
-            Size = info.Properties.System.Size.Value.ToString();
+            var inf = info.Properties.System.Size.Value;
+
+            if (inf == null) //данных нет
+            {
+                Size = inf.ToString();
+            }
+            else if (inf >= 1024*1024*1024) //гигабайт 
+            {
+                Size = (inf.Value/(1024*1024*1024)).ToString() + " GB";
+            }
+            else if (inf >= 1024) //пропускаем мегабайты для наглядности и проверяем килобайт
+            {
+                // в одном килобайте 1024 байт.
+                Size = (inf / 1024).ToString() + " KB";
+            }
+            else //помянем обладателей файлы в терабайты. Считаем что все остальное считать в байтах
+            {
+                Size = inf.ToString() + " B";
+            }
+            // Size = info.Properties.System.Size.Value.ToString();
         }
 
         public override string ToString()
@@ -210,7 +228,8 @@ namespace Archivator
     {
         ObservableCollection<ListItem> Entry;
         string CurrentDirectory;
-
+        //тест для многопоточности
+        Dispatcher Test;
         #region Вспомогательные функции
 
         /// <summary>
@@ -244,54 +263,67 @@ namespace Archivator
         /// <summary>
         /// Обновляет ListView
         /// </summary>
-        /// <param name="Path">Полный путь к директории, которую отображает ListView</param>
+        /// <param name="Path">Полный путь к директории, которую отображает Explorer (ListItem)</param>
         private void UpdateListView(string Path)
         {
-            // Проверяет есть ли вообще такая директория
-            // Способ не идеален. Directory.Exists не поддерживает пути которые являются ссылками или shell команды. Пример: C:\Users\mixap\ Папка загрузки, документы и так далее
-            // Костыль с shell
+            /* И ОПЯТЬ КОММЕНТАРИИ НЕ МОГУ СВЕРНУТЬ
+             Проверяет есть ли вообще такая директория
+             Способ не идеален. Directory.Exists не поддерживает пути которые являются ссылками или shell команды. Пример: C:\Users\mixap\ Папка загрузки, документы и так далее
+             Костыль с shell
+             */
+            System.Diagnostics.Stopwatch TestWatch = new System.Diagnostics.Stopwatch();
+            TestWatch.Start();
             if (Directory.Exists(Path) || Path.Contains("shell:"))
             {
-                CurrentDirectory = Path;
-                Navigator.Text = CurrentDirectory;
                 // значение не попадает в ожидаемый диапозон
-                var TargetFolder = (ShellFolder)ShellObject.FromParsingName(CurrentDirectory);
-                Entry.Clear();
-                foreach (var item in TargetFolder)
+                try
                 {
-                    // работай ок ?
-                    Entry.Add(new ListItem(item));
+                    var TargetFolder = (ShellFolder)ShellObject.FromParsingName(Path);
+                    CurrentDirectory = TargetFolder.ParsingName;
+                    Navigator.Text = CurrentDirectory;
+                    Entry.Clear();
+                    foreach (var item in TargetFolder)
+                    {
+                        // работай ок ?
+                        // вот это можно попробовать с помощью BeginInvoke сделать. Скорее всего это очень долго
+                        Test.InvokeAsync(() =>
+                        {
+                            Entry.Add(new ListItem(item));
+                        });
+                    }
+                    // вот Dispose для всей папки. Разницы между вызовом Dispose для каждого объекта и только для Target Folder ВРОДЕ нет.
+                    TargetFolder.Dispose();
                 }
-                // вот Dispose для всей папки. Разницы между вызовом Dispose для каждого объекта и только для Target Folder ВРОДЕ нет.
-                TargetFolder.Dispose();
+                catch (ShellException e)
+                {
+                    MessageBox.Show($"Произошла непредвиденная ошибка \n Код ошибки:{e.HResult}  Подробности:{e.Message}");
+                }
             }
             else
             {   // сообщение об ошибке
                 //Надо поменять стиль MessageBox
                 MessageBox.Show("Директория не найдена");
             }
+            TestWatch.Stop();
+            System.Diagnostics.Trace.WriteLine($"Всего времени прошло : {TestWatch.ElapsedMilliseconds}");
         }
-
         #endregion
-
 
         public MainWindow()
         {
             InitializeComponent();
-            /* Test List
-            List<ListItem> LSD = new List<ListItem>();
-            LSD.Add(new ListItem(new FileInfo(@"C:\Users\mixap\Downloads\witch_hunter_0.3.21-pc_0.zip"), System.Drawing.Icon.ExtractAssociatedIcon(@"C:\Users\mixap\Downloads\witch_hunter_0.3.21-pc_0.zip")));
-            LSD.Add(new ListItem(new FileInfo(@"C:\Users\mixap\Downloads\witch_hunter_0.3.21-pc_0.zip"), System.Drawing.Icon.ExtractAssociatedIcon(@"C:\Users\mixap\Downloads\witch_hunter_0.3.21-pc_0.zip")));
-            Explorer.ItemsSource = LSD;
-            */
-            // Первое заполнение ListView происходит при инициализации MainWindow (запуске приложения)
-            // ObservableCollection это тот же List только реализующий интерфейсы INotifyCollectionChanged, INotifyPropertyChanged .
-            // WPF использует их ( интерфейсы ) для обновления значений в ListView
 
             Entry = new ObservableCollection<ListItem>();
 
-            UpdateListView(@"shell:Downloads");
-
+            //UpdateListView(@"shell:Downloads"); // не используем потому что нужно инициировать CurrentDirectory
+            var Dir = (ShellFolder)ShellObject.FromParsingName("shell:Downloads");
+            CurrentDirectory = Dir.ParsingName;
+            Navigator.Text = CurrentDirectory;
+            foreach (var file in Dir)
+            {
+                Entry.Add(new ListItem(file));
+            }
+            Test = Application.Current.Dispatcher;
             Explorer.ItemsSource = Entry;
         }
 
@@ -317,13 +349,13 @@ namespace Archivator
 
         #endregion
 
-        // TextBox (который справа от здоровой стрелки) должен принимать на вход путь к папке или к файлу
+        // TextBox (который справа от здоровой стрелки, далее Navigator) должен принимать на вход путь к папке или к файлу
         // и либо вывести список файлов в папке либо архивировать ( возможно отдельное окно сделать под это надо)
         // НЕ ДОДЕЛАНО. Exceptions | ввод вместе с файлом
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
 
-            #region Комменты не сворачиваются ПОЧЕМУ-ТО
+            #region Комменты не сворачиваются ПОЧЕМУ-ТО. Не удаляю потому-что вся эта дичь с SLSID ключами всё ещё под вопросом
             /* 
              * На всякий случай не удаляю. Я думаю надо вообще 2 версии сделать
              * Одна будет через SLSID ключи, другая "По старинке" и проблему иконок решить путём "Не можем настоящую поставим дефолтную"
@@ -371,8 +403,19 @@ namespace Archivator
         private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             ListItem Src = (e.Source as ListViewItem).Content as ListItem;
-            // не очень красиво
-            UpdateListView(CurrentDirectory + @"\" + Src.Name);
+            /* "адрес" логического диска выглядит так - "C:\" 
+               и при этом чтобы получить путь до объекта нам нужно прибавить к его имени - "\"
+               получается строка "C:\\NAME" которая проходит проверку Directory.Exists,
+               но не проходит в ShellObject.FromParsingName
+            */
+            if (CurrentDirectory[CurrentDirectory.Length - 1] == '\\')
+            {
+                UpdateListView(CurrentDirectory + Src.Name);
+            }
+            else
+            {
+                UpdateListView(CurrentDirectory + @"\" + Src.Name);
+            }
             // Src - это вся необходимая о файле инфа
             // Можно открыть файл на чтение через 
             // var FileStr = File.OpenRead(Src.FullName);
@@ -383,24 +426,6 @@ namespace Archivator
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             UpdateListView(Directory.GetParent(CurrentDirectory).FullName);
-        }
-
-        // сохранить выделенный до этого объект (Я себя понял). Надо попробовать MultiBinding
-        private void ListViewItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            /* Это ломает Binding
-            var ob = sender as ListViewItem;
-            if(mem == null)
-            {
-                mem = ob;
-            }
-            else
-            {
-                mem.Background = new SolidColorBrush(Colors.Transparent);
-                mem = ob;
-            }
-            ob.Background = Resources["FadedBlue"] as System.Windows.Media.Brush;
-            */
         }
 
     }
